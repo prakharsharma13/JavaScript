31e874212e9f19e68f8897005fc3e397fac856a6
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-07-10 10:28:20 +0530

    Stack and Heap memory in JS

---
+++ 01_basics/memory.js
@@ -0,0 +1,21 @@
+//  Stack(primitive) Heap(Non-primitive)
+
+let myName = "PrakharSharma";
+
+let anotherName = myName;
+anotherName = "chaiaurcode"
+
+console.log(myName);
+console.log(anotherName)
+
+let userOne = {
+    email: "userone@gmail.com",
+    upi: "userone@ybl"
+}
+let userTwo = {
+    email: "usertwo@gmail.com",
+    upi: "usertwo@ybl"
+}
+
+console.log(userOne.email)
+console.log(userTwo.email)
